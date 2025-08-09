1ba4cf50305d2c0713ca634d336985ce15268e6a
Eko Widodo <ekowdd89@gmail.com>
2025-08-09 11:11:19 +0700

    tracer

--- cmd/author/main.go
+++ cmd/author/main.go
@@ -17,8 +17,6 @@ func main(){
 	if err != nil {
 		panic(err)
 	}
-	// postgres://testing:testing@postgresql:5434/testing?sslmode=disable
-	// postgres://postgres:cempakamasp23@127.0.0.1:5434/sertif_tes?sslmode=disable
 	p,err:= db.New(
 		db.WithConnectionString(os.Getenv("DB_URL")),
 		db.WithTracer("main postgres"),
@@ -39,22 +37,5 @@ func main(){
 	if err := h.Start(context.Background()); err != nil {
 		h.Close(context.Background())
 	}
-	fmt.Println("server started listening on port 8080")
-	// g:= gin.Default()
-	// g.GET("/", func(ctx *gin.Context) {
-	// 	otel.InitTracer(context.Background())
-	// 	_, err:= db.New(
-	// 		db.WithConnectionString("postgres://testing:testing@postgresql:5434/testing?sslmode=disable"),
-	// 		db.WithTracer("main postgres"),
-	// 	)
-	// 	if err !=nil {
-	// 		fmt.Println(err.Error())
-	// 	}
-	// 	ctx.JSON(200, gin.H{
-	// 		"message": "pong",
-	// 	})
-	// })
-	// if err:= g.Run(fmt.Sprintf(":%s", os.Getenv("APP_LISTEN"))); err != nil {
-	// 	panic(err)
-	// }
+	fmt.Println("server started listening on port ", os.Getenv("APP_LISTEN"))
 }
